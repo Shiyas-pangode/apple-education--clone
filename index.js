bb21cf4cb9adb5977b735a3236154ca087ae8aef
shiyas-pangode <shiyasschachu@gmail.com>
2024-08-03 19:06:40 +0530

    https://github.com/Shiyas-pangode/apple-education--clone

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
     loop: false,
     slidesPerView: 3.5,
     spaceBetween: 10,
     
 
-    // Navigation arrows
+
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
     },
-
-
 });   
 
 
@@ -22,26 +19,43 @@ document.addEventListener("DOMContentLoaded", function() {
     let closebar = document.querySelector('.close-icon');
     let carticon = document.querySelector('.cart')
     let nav3 = document.querySelector('#nav-three');
+    let navFour = document.querySelector('.navthree-ul')
     function opensearch() {
         searchbar.style.display = 'flex';
-        searchbar.style.csstext = "animation-name:slideIn";
+        searchbar.classList.add('show');
+        searchbar.classList.remove('hide');
+        searchbar.style.animation = "slideIn 0.5s forwards";
     }
     function closesearch(){
-        
-        searchbar.style.display = 'none';      
+        searchbar.classList.add('show');
+        searchbar.classList.remove('hide');
+        searchbar.style.animation = "slideOut 0.5s forwards";
+        setTimeout (() => {
+            searchbar.style.display = "none";
+        },500);
     }
     function openshop() {
         nav3.style.display = 'flex';
     }
     function closeshop () {
-        nav3.style.display = 'none';
+        setTimeout(()=>{
+            nav3.style.display = 'none';
+        },1000);
+    }
+    function open() {
+        nav3.style.display = 'flex';
     }
 
     icon.addEventListener('click', opensearch);
     closebar.addEventListener('click',closesearch);
     carticon.addEventListener('mouseover',openshop);
-    carticon.addEventListener('mouseout',closeshop);
+    navFour.addEventListener('mouseout',closeshop);
+
+    let searchInput = document.getElementsByClassName('input');
+    function searchinput(){
+        
+    } 
+    searchInput.addEventListener('input',searchinput);
    
 });
 
-  
